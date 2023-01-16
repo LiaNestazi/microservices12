e6fd21c1db26026422f1ba4b07b0e28e7d1d0aee
emiliamin <anastasia.lijigoryaeva@mail.ru>
2023-01-16 13:51:36 +0300

    Add tests

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@postgres:5432/microdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@172.17.0.2:5432/microdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ tests/test_main.py
@@ -0,0 +1,36 @@
+import requests
+
+api_url = 'http://localhost:8000'
+
+def test_healthcheck():
+    response = requests.get(f'{api_url}/__health')
+    assert response.status_code == 200
+
+
+class testMenuItems():
+    def test_get_blank_items():
+        response = requests.get(f'{api_url}/v1/menu_items')
+        assert response.status_code == 200
+        assert len(response.json()) == 0
+    
+    def test_add_item():
+        body = {"name": "item 1", "description": "desc 1", "price": 350 }
+        response = requests.post(f'{api_url}/v1/menu_items', json = body)
+        assert response.status_code == 200
+        assert response.json().get('name') == 'item 1'
+        assert response.json().get('description') == 'desc 1'
+        assert response.json().get('price') == 350
+        assert response.json().get('id') == 0
+
+    def test_get_item_id():
+        response = requests.post(f'{api_url}/v1/menu_items/0')
+        assert response.status_code == 200
+        assert response.json().get('name') == 'item 1'
+        assert response.json().get('description') == 'desc 1'
+        assert response.json().get('price') == 350
+        assert response.json().get('id') == 0
+
+    def test_get_items():
+        response = requests.get(f'{api_url}/v1/menu_items')
+        assert response.status_code == 200
+        assert len(response.json()) == 1
